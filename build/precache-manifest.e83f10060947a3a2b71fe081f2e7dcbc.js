5c98c560e9c0e39d04b5983827d9e091fae2abbf
No1RajAryan <55110091+No1RajAryan@users.noreply.github.com>
2020-09-24 11:49:54 +0530

    fvideoapp

---
+++ build/precache-manifest.e83f10060947a3a2b71fe081f2e7dcbc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906f9c98762a0c3e209b840c08110c73",
+    "url": "/videoappfront/build/index.html"
+  },
+  {
+    "revision": "f2ee794c5afc59598598",
+    "url": "/videoappfront/build/static/css/main.a5fe1156.chunk.css"
+  },
+  {
+    "revision": "496e3b4f999f8ffea2d5",
+    "url": "/videoappfront/build/static/js/2.e4ab4134.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/videoappfront/build/static/js/2.e4ab4134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2ee794c5afc59598598",
+    "url": "/videoappfront/build/static/js/main.924eff8e.chunk.js"
+  },
+  {
+    "revision": "545d2fda34307ac95277",
+    "url": "/videoappfront/build/static/js/runtime-main.be4cb9f6.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.730f479554ef2dbd5978c83eef8ad070.js"
+  "/videoappfront/build/precache-manifest.e83f10060947a3a2b71fe081f2e7dcbc.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/videoappfront/build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
