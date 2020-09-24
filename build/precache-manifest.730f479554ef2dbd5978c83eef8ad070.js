24d72c01948277fa915021e3364d2be3ebca51ff
No1RajAryan <55110091+No1RajAryan@users.noreply.github.com>
2020-09-24 11:15:41 +0530

    fvideoapp

---
+++ build/precache-manifest.730f479554ef2dbd5978c83eef8ad070.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "413e2f7a9f4639e237f2d9156d6dea1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "afeab4f45796b09e17cb",
+    "url": "/static/css/main.a5fe1156.chunk.css"
+  },
+  {
+    "revision": "496e3b4f999f8ffea2d5",
+    "url": "/static/js/2.e4ab4134.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e4ab4134.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afeab4f45796b09e17cb",
+    "url": "/static/js/main.537be7b4.chunk.js"
+  },
+  {
+    "revision": "2b74d1f254f04f0a73c1",
+    "url": "/static/js/runtime-main.30bc3337.js"
+  }
+]);
